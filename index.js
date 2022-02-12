d7fdb70271fe2bd35a3fd4cc10805ee2b935ee7c
Cristian <cristiandescosido@gmail.com>
2022-02-12 10:20:40 -0300

    Añadida logica del start/restart button

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ let direction = 1;
 const width = 10;
 let appleIndex = 0;
 let score = 0;
+let intervalTime = 1000;
+let speed = 0.9;
+let timerId = 0;
 
 function createGrid () {
     for(let i = 0; i < width*width; i++) {
@@ -20,6 +23,22 @@ function createGrid () {
  createGrid();
 
  currentSnake.forEach(index => squares[index].classList.add('snake'));
+
+
+ function startGame() {
+    currentSnake.forEach(index => squares[index].classList.remove('snake'));
+    squares[appleIndex].classList.remove('apples');
+    clearInterval(timerId);
+    currentSnake = [2,1,0];
+    direction = 1;
+    score = 0;
+    scoreDisplay.textContent = score;
+    intervalTime = 1000;
+    speed = 0.9;
+    generateApples();
+    currentSnake.forEach(index => squares[index].classList.add('snake'));
+    timerId = setInterval(move, intervalTime);
+ }   
 
  function move() {
      if (
@@ -43,13 +62,11 @@ function createGrid () {
          generateApples();
          score += 10;
          scoreDisplay.textContent = score;
-
+         clearInterval(timerId);
+         intervalTime *= speed;
+         timerId = setInterval(move, intervalTime);
      }
  }
-
- move();
-
-let timerId = setInterval(move, 1000);
 
 function generateApples () {
     do {
@@ -64,18 +81,15 @@ generateApples();
 
  function control (e) {
      if (e.keyCode === 39) {                   // 39 is right arrow
-         console.log("you're going right")
          direction = 1;
      } else if (e.keyCode === 37){             // 37 is for the left arrow
-        console.log("you're going left")
         direction = -1;
      } else if (e.keyCode === 38){             // 38 is for the up arrow
-        console.log("you're going up")
         direction = -width;
      } else if (e.keyCode === 40){             // 40 is for the down arrow
-        console.log("you're going down")
         direction = +width;
      }
  }
 
- document.addEventListener('keydown', control)
+ document.addEventListener('keydown', control);
+ startBtn.addEventListener('click', startGame);
